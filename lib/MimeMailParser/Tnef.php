6fd57b401ea358922fd3c6cc968e1552f93e1575
chengshq <csq1124@gmail.com>
2015-01-22 10:41:56 +0800

    add MimeMailParser with Tnef

---
+++ lib/MimeMailParser/Tnef.php
@@ -0,0 +1,13 @@
+<?php
+
+include_once __DIR__ . '/Tnef/functions.php';
+include_once __DIR__ . '/Tnef/constants.php';
+include_once __DIR__ . '/Tnef/tnef.php';
+
+include_once __DIR__ . '/Tnef/tnef_attachment.php';
+include_once __DIR__ . '/Tnef/tnef_date.php';
+include_once __DIR__ . '/Tnef/tnef_mailinfo.php';
+include_once __DIR__ . '/Tnef/tnef_file_base.php';
+include_once __DIR__ . '/Tnef/tnef_file.php';
+include_once __DIR__ . '/Tnef/tnef_file_rtf.php';
+include_once __DIR__ . '/Tnef/tnef_vcard.php';
---
+++ lib/Parser.php
@@ -0,0 +1,4 @@
+<?php
+require_once __DIR__ . '/MimeMailParser/Parser.php';
+require_once __DIR__ . '/MimeMailParser/Attachment.php';
+require_once __DIR__ . '/MimeMailParser/Tnef.php';
